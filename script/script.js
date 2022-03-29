8fc15528fe47fd5a84561b02a179bed7dc526560
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2022-03-29 16:14:00 +0200

    fixed nav, edu and other

---
+++ script/script.js
@@ -0,0 +1,16 @@
+function dropNav() {
+    let drop = document.getElementById("drop-nav");
+    if (drop.className === "dropdown-list") {
+    drop.className += " open";
+    } else {
+    drop.className = "dropdown-list";
+    }
+}
+function burgerMenu() {
+      let burger = document.getElementById("nav-bar");
+      if (burger.className === "nav-list") {
+      burger.className += " responsive";
+      } else {
+      burger.className = "nav-list";
+      }
+}
