cde884f1c2a2483531e397f0bab086a26f72a5c7
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2022-04-05 11:21:56 +0200

    testing again form

--- form.php
+++ form.php
@@ -1,4 +1,33 @@
 <?php
-    mail("kicki.lindstrand@gmail.com", "Subject", "Message");
+
+$to = "din_egen@e-postadress_här";
+$from = $_POST["email"];
+$subject = 'Kontakt från webbplatsen!';
+$fnamn = $_POST["fnamn"];
+$enamn = $_POST["enamn"];
+$message = $_POST["message"];
+
+##########################
+// HEADERS för innehållstyp och textkodning
+$headers = "Content-Type: text/plain; charset=utf-8 \r\n";
+$headers .= "From:".$fnamn." ".$enamn." <".$from.">"."\r\n";
+$headers .= "MIME-Version: 1.0 \r\n";
+###########################
+
+// Konverterar ÅÄÖåäö till UTF-8
+$subject='=?UTF-8?B?'.base64_encode($subject).'?=';
+
+// Mailfunktionen som skickar bekräftelsen
+if (mail($to, $subject, $message, $headers))
+
+echo nl2br("<h2>Ditt meddelande har skickats!</h2>
+<b>mottagare:</b> $to
+<b>meddelande:</b>
+$message
+");
+
+else
+echo "Det gick inte att skicka ditt meddelande";
+}
 
 ?>
