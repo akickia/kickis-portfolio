448cd42fc35995d9c2f20b17d35ccfa1bc6e39df
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2022-04-05 11:04:12 +0200

    test4 form

--- script/form.php
+++ script/form.php
@@ -1,19 +1,4 @@
 <?php
-    $name = $_POST['name']; 
-    $mailFrom = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+    mail("kicki.lindstrand@gmail.com", "Subject", "Message");
 
-    $email_from = 'kicki.lindstrand@gmail.com'; 
-    $email_subject = "New Form Submission from Portfolio"; 
-    $headers = "From: My Portfolio ".$mailFrom; 
-    $email_body = "User Name: $name. \n". 
-                  "User Email: $mailFrom. \n". 
-                  "User Subject: $subject. \n". 
-                  "User Message: $message. \n.";   
-
-    $to = "kicki.lindstrand@gmail.com"; 
-    $headers = "From: $email_from \r\n"; 
-    mail($to,$email_subject,$email_body,$headers); 
-    header("Location:../pages/contact.html"); 
 ?>
