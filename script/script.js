15b3ac8f4e64f1fe5d1f5408c3872c4bafefdd1e
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2022-05-13 11:28:03 +0200

    fixed functions in about-page

--- script/script.js
+++ script/script.js
@@ -18,12 +18,46 @@ function burgerMenu() {
 
 
 
-const mores = document.querySelectorAll('.more');
-const readMore = document.querySelectorAll('.readMore')
 
+/*
 mores.forEach((more) => {
     more.addEventListener('click', () => {
         more.classList.add('expand');
-        readMore.innerHTML = ""
     })
-})
+})*/
+
+function openMore1() {
+    let more = document.getElementById("more1");
+    let readMore = document.getElementById('readMore1')
+    if (more.className === "more1") {
+    more.className += " expand";
+    readMore.innerHTML = "Click to hide";
+    } else {
+    more.className = "more1";
+    readMore.innerHTML = "Click to read more";
+    }
+}
+
+function openMore2() {
+    let more = document.getElementById("more2");
+    let readMore = document.getElementById('readMore2')
+    if (more.className === "more2") {
+    more.className += " expand";
+    readMore.innerHTML = "Click to hide";
+    } else {
+    more.className = "more2";
+    readMore.innerHTML = "Click to read more";
+    }
+}
+
+function openMore3() {
+    let more = document.getElementById("more3");
+    let readMore = document.getElementById('readMore3')
+    if (more.className === "more3") {
+    more.className += " expand";
+    readMore.innerHTML = "Click to hide";
+    } else {
+    more.className = "more3";
+    readMore.innerHTML = "Click to read more";
+    }
+}
